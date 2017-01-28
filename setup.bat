a7e1adaa0f1d85bdd8d4346621442b63e2b82dd6
tig@example.com <tig@example.com>
2017-01-28 15:59:17 +0900

    cinstをコメントアウト

--- setup.bat
+++ setup.bat
@@ -1,4 +1,8 @@
-cinst -y 7z wget &&\
-wget https://github.com/Maximus5/ConEmu/releases/download/v17.01.18/ConEmuPack.170118.7z &&\
-%chocolateyinstall%\tools\7z x ConEmuPack.170118.7z &&\
+REM cinstは管理者権限で動作させる必要があるが
+REM 管理者権限に昇格すると実行パスがc:\system32とかになって面倒なのでコメントアウト
+REM 実行時は一般ユーザ権限で実行すること。
+REM cinst -y 7z wget &&^
+wget https://github.com/Maximus5/ConEmu/releases/download/v17.01.18/ConEmuPack.170118.7z &&^
+%chocolateyinstall%\tools\7z x ConEmuPack.170118.7z &&^
 del ConEmuPack.170118.7z
+PAUSE
